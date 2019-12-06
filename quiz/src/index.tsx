40816428ec4cb2226c6f6651d3c25cc368f6df73
Miccili <wot.dannbo@gmail.com>
2019-12-06 11:44:40 +0900

    細かい所をFIX

--- quiz/src/index.tsx
+++ quiz/src/index.tsx
@@ -1,51 +1,46 @@
 import * as React from 'react';
-import { useState, useCallback } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import data from './data.json'
+import {useState, useCallback} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import data from './data.json';
 
 type AnswerProps = {
-  key: number
-  text: string
-  clickItem: (text: string) => void
-}
+  key: number;
+  text: string;
+  clickItem: (text: string) => void;
+};
 
 type CorrectProps = {
-  is_correct: boolean
-}
+  is_correct: boolean;
+};
 
 type QuizProps = {
-  data: any //JSONの型闇では？？
-}
+  data: any; //JSONの型闇では？？
+};
 
 const Answer: React.FC<AnswerProps> = props => {
   return (
-    <li
-      key={props.key}
-      onClick={() => props.clickItem(props.text)}
-    >
+    <li key={props.key} onClick={() => props.clickItem(props.text)}>
       {props.text}
     </li>
-  )
-}
+  );
+};
 
 const CorrectAnswer: React.FC<CorrectProps> = props => {
   return (
     <div className="sample">
-      <div>{props.is_correct ? "正解" : "不正解"}</div>
+      <div>{props.is_correct ? '正解' : '不正解'}</div>
       <div>答え: {props.children}</div>
     </div>
-  )
-}
+  );
+};
 
 const Quiz: React.FC<QuizProps> = props => {
-  const [quizList, setQuizList] = useState(props.data);
   const [pageNumber, setPageNumber] = useState(0);
   const [isAnswer, setIsAnswer] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
 
   useCallback(() => {
-    setQuizList(props.data);
     setIsAnswer(false);
     setIsCorrect(false);
   }, [props.data]);
@@ -76,17 +71,15 @@ const Quiz: React.FC<QuizProps> = props => {
         })}
       </ul>
       {isAnswer && (
-        <CorrectAnswer
-          is_correct={isCorrect}
-        >{data.quiz[pageNumber].answer[data.quiz[pageNumber].correct]}</CorrectAnswer>
+        <CorrectAnswer is_correct={isCorrect}>
+          {data.quiz[pageNumber].answer[data.quiz[pageNumber].correct]}
+        </CorrectAnswer>
       )}
-      {
-        quizList.length - 1 !== pageNumber && (
-          <button onClick={() => chanageNextPage()}>次へ</button>
-        )
-      }
+      {data.quiz.length - 1 !== pageNumber && (
+        <button onClick={() => chanageNextPage()}>次へ</button>
+      )}
     </section>
-  )
-}
+  );
+};
 
-ReactDOM.render(<Quiz data={data}/>, document.getElementById('root'));
+ReactDOM.render(<Quiz data={data} />, document.getElementById('root'));
